47faf0bb9d0e766b7ac3a38df5d325e84eb52de6
Simon Godden <sgodden@gmail.com>
2011-10-14 19:01:55 +0100

    latest stuff

---
+++ templates/javaee-app/webapp/src/main/webapp/app/controller/Users.js
@@ -0,0 +1,19 @@
+Ext.define('AM.controller.Users', {
+    extend: 'Ext.app.Controller',
+
+    views: [
+        'user.List'
+    ],
+
+    init: function() {
+        this.control({
+            'viewport > panel': {
+                render: this.onPanelRendered
+            }
+        });
+    },
+
+    onPanelRendered: function() {
+        console.log('The panel was rendered');
+    }
+});
