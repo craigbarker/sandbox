5506e39c46b1d2a0949ef1b2eab2051df00816db
Simon Godden <sgodden@gmail.com>
2012-01-19 07:29:40 +0000

    Converted unit test to scala

---
+++ templates/javaee-app/model/src/test/scala/org/sgodden/tom/model/CustomerOrderTest.scala
@@ -0,0 +1,47 @@
+package org.sgodden.tom.model
+
+import org.testng.annotations.Test
+import org.testng.Assert._
+import java.util.Calendar
+import javax.validation.{ConstraintViolation, Validation}
+
+@Test
+class CustomerOrderTest {
+
+  private val validator = Validation.buildDefaultValidatorFactory.getValidator
+
+  def testValidation {
+    assertEquals(validator.validate(new CustomerOrder).size, 2, "Wrong number of violations")
+  }
+
+  def customerReferenceMustNotBeNull {
+    assertTrue(containsViolation(getViolations(new CustomerOrder), "customerReference", "may not be null"))
+  }
+
+  def customerReferenceMustBeginWithCr {
+    val order = new CustomerOrder {
+      setOrderNumber("ordnum")
+      setCustomerReference("foo")
+      setBookingDate(Calendar.getInstance)
+    }
+    // TODO - message should be i18nd
+    assertTrue(containsViolation(getViolations(order), "customerReference", "customerReferenceMustBeginWithCr"))
+
+    order.setCustomerReference("cr001")
+    assertFalse(containsViolation(getViolations(order), "customerReference", "customerReferenceMustBeginWithCr"))
+  }
+
+  private def getViolations(order: CustomerOrder) = {
+    val rawViolations = validator.validate(order)
+    rawViolations.toArray[ConstraintViolation[CustomerOrder]](
+      new Array[ConstraintViolation[CustomerOrder]](rawViolations.size))
+  }
+
+  private def containsViolation(
+                                 violations: Array[ConstraintViolation[CustomerOrder]],
+                                 path: String,
+                                 message: String): Boolean = {
+    violations.filter(cv => path == cv.getPropertyPath.toString && message == cv.getMessage).size > 0
+  }
+
+}
