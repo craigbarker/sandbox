040da8c9eb2b8a540e0a39d5506aa2a5718236b4
Simon Godden <sgodden@gmail.com>
2012-05-03 07:27:53 +0100

    sorting working again

--- templates/dojo-java/webapp/src/main/webapp/purchaseOrdersDataGrid.js
+++ templates/dojo-java/webapp/src/main/webapp/purchaseOrdersDataGrid.js
@@ -8,41 +8,21 @@ require(["dijit/dijit", "dojo/store/JsonRest", "dojo/parser",
     function(dijit, JsonRest, parser, Observable, dom, on, domConstruct, fx, stamp, locale, dojoDate, DataGrid,
         ObjectStore) {
 
-//        poStore = new JsonRest({
-//            "target": "services/purchase-orders/",
-//            "idProperty": "id"
-//        });
-//
-//        poStore.query();
-//
-//        var formatDate = function(raw) {
-//            var forecastDate = stamp.fromISOString(raw);
-//            return locale.format(forecastDate, {
-//                "formatLength": "medium",
-//                "selector": "date"
-//            });
-//        };
-//
-//        var layout = [[
-//            {'name': 'Part', 'field': 'part', 'width': '10em'},
-//            {'name': 'Description', 'field': 'description', 'width': '20em', 'sortable': false},
-//            {'name': 'PO Number', 'field': 'poNumber'},
-//            {'name': 'Committed Qty', 'field': 'committedQuantity'},
-//            {'name': 'Committed Date', 'field': 'committedDate', 'width': '10em', 'formatter': formatDate},
-//            {'name': 'Forecast Qty', 'field': 'forecastQuantity'},
-//            {'name': 'Forecast Date', 'field': 'forecastDate', 'width': '10em', 'formatter': formatDate}
-//        ]];
-//
-//        var grid = new DataGrid({
-//            'id': 'purchaseOrderGrid',
-//            'store': new ObjectStore({'objectStore': poStore}),
-//            'structure': layout,
-//            'rowSelector': '20px',
-//            'loadingMessage': 'Loading'
-//        }, document.createElement('div'));
-//
-//        var elem = dojo.byId('grid-container');
-//        elem.appendChild(grid.domNode);
-//
-//        grid.startup();
+        formatDate = function(raw) {
+            var forecastDate = stamp.fromISOString(raw);
+            return locale.format(forecastDate, {
+                "formatLength": "medium",
+                "selector": "date"
+            });
+        };
+
+        poStore = new JsonRest({
+            "target": "services/purchase-orders/",
+            "idProperty": "id"
+        });
+
+        objectStore = new ObjectStore({'objectStore': poStore});
+
+        parser.parse();
+
     });
